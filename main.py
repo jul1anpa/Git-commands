c157b786aac30162ef76e87042f1f40f665565d9
jul1anpa <paynejulian1998@gmail.com>
2023-08-24 09:36:44 -0400

    Added comments -jp

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+'''
+You can use git config --global 
+user.name to change a "username"
+user.email to change an "email"
+
+git init will initialize a new git repository
+git status will return the your branch status
+git add . will add all commits to the added queue
+git commit will commit all queued commits
+
+'''
+
 print("Hello World")
+print("Goodbye")
